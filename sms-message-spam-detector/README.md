df7809bfccef11799e168d65d04de3f78cd83563
EC2 Default User <ec2-user@ip-172-31-41-231.us-east-2.compute.internal>
2020-04-24 21:44:40 +0000

    first commit

---
+++ sms-message-spam-detector/README.md
@@ -0,0 +1,1 @@
+# Flask-Application_aws
---
+++ sms-message-spam-detector/run_docker.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Build image
+docker build --tag=app .
+
+# List docker images
+docker image ls
+
+# Run flask app
+docker run -p 8080:8080 app
